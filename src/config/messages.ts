3ed5946dbef4c0d38b3288c3201f576e2f9e1882
Sampaio Leal <samp14hd@gmail.com>
2021-12-23 11:56:38 -0300

    Update src/config/messages.ts
    Co-authored-by: Gabriel Silva <devzgabriel@gmail.com>

--- src/config/messages.ts
+++ src/config/messages.ts
@@ -1,4 +1,4 @@
-const MESSAGES = {
+const ERROR_MESSAGES = {
   invalid_email: 'E-mail invalido',
   minimum_password: 'A senha deve conter, no minimo, 8 caracteres',
   required: 'Campo obrigatório',
