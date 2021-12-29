04a81612d1220cb513e099db44beadc3391296f9
Sampaio Leal <samp14hd@gmail.com>
2021-12-29 16:49:08 -0300

    fix: api services adapted to nest-seed

--- src/modules/users/repository.ts
+++ src/modules/users/repository.ts
@@ -3,7 +3,7 @@ import httpService from 'services/httpService';
 import { Repository } from '@euk-labs/fetchx';
 
 const usersRepository = new Repository(httpService, {
-  path: '/api/users',
+  path: '/users',
 });
 
 export default usersRepository;
--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,7 +1,11 @@
+import getConfig from 'next/config';
+
 import { HttpService } from '@euk-labs/fetchx';
 
+const { publicRuntimeConfig } = getConfig();
+
 const httpService = new HttpService({
-  baseURL: 'http://localhost:3000',
+  baseURL: publicRuntimeConfig.apiUrl,
 });
 
 export default httpService;
