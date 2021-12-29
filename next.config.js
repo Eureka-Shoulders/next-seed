b6028015edce09eb84fee226c02193137f5bbdd0
Sampaio Leal <samp14hd@gmail.com>
2021-12-29 16:47:44 -0300

    chore: updating dependencies and configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(
+  ['@euk-labs/formix', '@euk-labs/componentz', '@euk-labs/fetchx'],
+  {
+    resolveSymlinks: true,
+  }
+);
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPlugins([withBundleAnalyzer, withTM], {
   reactStrictMode: true,
   publicRuntimeConfig: {
-    apiUrl: process.env.API_URL || 'http://localhost:3000',
+    apiUrl: process.env.API_URL || 'http://localhost:3030',
     useMirage: process.env.USE_MIRAGE || false,
   },
-};
+});
