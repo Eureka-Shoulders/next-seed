14cf8d1058e5f365a087c78d6123d8667291b642
Sampaio Leal <samp14hd@gmail.com>
2021-12-30 18:59:56 -0300

    fix: drawer default pages

--- src/config/pages.ts
+++ src/config/pages.ts
@@ -1,12 +1,11 @@
 import UserIcon from '@mui/icons-material/AccountCircle';
 import Dashboard from '@mui/icons-material/Dashboard';
-import MailIcon from '@mui/icons-material/Mail';
 
 import { Page } from '@euk-labs/componentz/components/AppBar/types';
 
 export const pages: Page[] = [
   {
-    label: 'Home',
+    label: 'Início',
     link: '/',
     Icon: Dashboard,
   },
@@ -24,20 +23,8 @@ export const pages: Page[] = [
     ],
   },
   {
-    label: 'Server-Side Rendering page',
+    label: 'Server-Side',
     link: '/server',
     Icon: UserIcon,
-  },
-  {
-    label: 'E-mails',
-    link: '/emails',
-    Icon: MailIcon,
-    sub: [
-      {
-        label: 'Novo',
-        link: '/emails/new',
-        Icon: MailIcon,
-      },
-    ],
   },
 ];
