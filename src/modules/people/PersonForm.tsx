79668d79b82787b711fa30293154584d0a8d056d
Sampaio Leal <samp14hd@gmail.com>
2022-02-01 15:14:08 -0300

    fix: person form

--- src/modules/people/PersonForm.tsx
+++ src/modules/people/PersonForm.tsx
@@ -16,22 +16,22 @@ export default function PersonForm() {
       <Grid item xs={6}>
         <FXTextField name="name" label="Nome" />
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={6}>
+        <FXDatePicker
+          name="birthDate"
+          label="Data de Nascimento"
+          inputFormat="dd/MM/yyyy"
+        />
+      </Grid>
+      <Grid item xs={6}>
         <FXAutocomplete
           options={personTypes}
           name="type"
           label="Tipo de pessoa"
         />
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={6}>
         <FXCPFCNPJField name="identifier" typeField="type" />
-      </Grid>
-      <Grid item xs={12}>
-        <FXDatePicker
-          name="birthDate"
-          label="Data de Nascimento"
-          inputFormat="dd/MM/yyyy"
-        />
       </Grid>
     </Grid>
   );
